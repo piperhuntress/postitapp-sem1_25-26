0d95fbb7ba044437e3aa7e4cf0e2bc7d20543eeb
piperhuntress <jasminetumulak@gmail.com>
2025-10-09 09:37:26 +0400

    add and delete

--- client/src/Components/Register.js
+++ client/src/Components/Register.js
@@ -2,6 +2,7 @@ import { userSchemaValidation } from "../Validations/UserValidations";
 import * as yup from "yup";
 
 import { useForm } from "react-hook-form";
+import { useState } from "react";
 
 import { yupResolver } from "@hookform/resolvers/yup";
 import {
@@ -15,11 +16,19 @@ import {
   Form,
 } from "reactstrap";
 
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { addUser, deleteUser } from "../Features/UserSlice";
+
 //For form validation using react-hook-form
 
 const Register = () => {
   const userList = useSelector((state) => state.users.value);
+
+  //Declare your state variables
+  const [name, setname] = useState("");
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  const [confirmPassword, setconfirmPassword] = useState("");
 
   const {
     register,
@@ -32,26 +41,60 @@ const Register = () => {
   });
 
   // Handle form submission
+  const dispatch = useDispatch();
 
   const onSubmit = (data) => {
-    console.log("Form Data", data);
-    alert("Validation all good."); // You can handle the form submission here
+    try {
+      console.log("Form Data", data);
+      alert("Validation all good."); // You can handle the form submission here
+      const userData = {
+        name: data.name,
+        email: data.email,
+        password: data.password,
+      };
+      dispatch(addUser(userData));
+    } catch (error) {
+      console.log(error);
+    }
   };
 
+  const handleDelete = (email) => {
+    try {
+      dispatch(deleteUser(email));
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <Container fluid>
       <Form className="div-form" onSubmit={handleSubmit(onSubmit)}>
         <Row>
           <Col md={6}>
             Name<br></br>
-            <input type="text" name="name" {...register("name")}></input>
+            <input
+              type="text"
+              name="name"
+              {...register("name", {
+                value: name,
+                onChange: (e) => setname(e.target.value),
+              })}
+            ></input>
+            {name}
           </Col>
           <p className="error">{errors.name?.message}</p>
         </Row>
         <Row>
           <Col md={6}>
             Email<br></br>
-            <input type="email" name="email" {...register("email")}></input>
+            <input
+              type="email"
+              name="email"
+              {...register("email", {
+                value: email,
+                onChange: (e) => setemail(e.target.value),
+              })}
+            ></input>
+            {email}
           </Col>
           <p className="error">{errors.email?.message}</p>
         </Row>
@@ -61,8 +104,12 @@ const Register = () => {
             <input
               type="password"
               name="password"
-              {...register("password")}
+              {...register("password", {
+                value: password,
+                onChange: (e) => setpassword(e.target.value),
+              })}
             ></input>
+            {password}
           </Col>
           <p className="error">{errors.password?.message}</p>
         </Row>
@@ -72,8 +119,12 @@ const Register = () => {
             <input
               type="password"
               name="confirmpassword"
-              {...register("confirmPassword")}
+              {...register("confirmPassword", {
+                value: confirmPassword,
+                onChange: (e) => setconfirmPassword(e.target.value),
+              })}
             ></input>
+            {confirmPassword}
           </Col>
           <p className="error">{errors.confirmPassword?.message}</p>
         </Row>
@@ -103,7 +154,12 @@ const Register = () => {
                   <td>{user.password}</td>
                   <td>
                     <Button color="success">Update</Button>{" "}
-                    <Button color="danger">Delete</Button>
+                    <Button
+                      color="danger"
+                      onClick={() => handleDelete(user.email)}
+                    >
+                      Delete
+                    </Button>
                   </td>
                 </tr>
               ))}
--- client/src/Features/UserSlice.js
+++ client/src/Features/UserSlice.js
@@ -6,6 +6,16 @@ const initialState = { value: UsersData };
 export const userSlice = createSlice({
   name: "users", //name of the state
   initialState, // initial value of the state
-  reducers: {},
+  reducers: {
+    addUser: (state, action) => {
+      state.value.push(action.payload);
+    },
+    deleteUser: (state, action) => {
+      state.value = state.value.filter((user) => user.email !== action.payload);
+    },
+  },
 });
+
+export const { addUser, deleteUser } = userSlice.actions;
+
 export default userSlice.reducer;

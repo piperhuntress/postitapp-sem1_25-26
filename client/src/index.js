4132db2dd13eae4c8574ce1569b0182778521037
piperhuntress <jasminetumulak@gmail.com>
2025-11-17 13:42:47 +0400

    Redux Persist

--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,16 @@ import App from "./App";
 import { store } from "./Store/store";
 import { Provider } from "react-redux";
 import Register from "./Components/Register";
+import { persistore } from "./Store/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <React.StrictMode>
-      <App />
-      {/* <Register /> */}
+      <PersistGate loading={null} persistor={persistore}>
+        <App />
+      </PersistGate>
     </React.StrictMode>
   </Provider>
 );
